92af54f5e2fe9d6a3bc8c22e493ff44054d5ced1
fred <fred@172.31.99.229>
2019-01-13 16:35:34 -0800

    Initial maven app.

---
+++ score-keeper-sandbox/src/main/java/org/acm/barrie/sandbox/scorekeeper/App.java
@@ -0,0 +1,13 @@
+package org.acm.barrie.sandbox.scorekeeper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
